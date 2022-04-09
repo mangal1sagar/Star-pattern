d340e005caaaf18c47ea9eb8c39bfcd5ca15c0ae
unknown <sagar.t.mangal@gmail.com>
2022-04-09 17:13:36 +0530

    first commit

---
+++ starpattern.js
@@ -0,0 +1,10 @@
+// if else for loop
+
+//star pattern - triangle
+
+for (i = 0; i <= 9; i++) {
+    for (j = 0; j <= i; j++) {
+        document.write("*" + "&nbsp")
+    }
+    document.write("<br>")
+}
